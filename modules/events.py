9dff93452b4338b27c7b76ca5f2f9db85d7ce2df
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-04-24 21:12:40 -0500

    make it better

--- modules/events.py
+++ modules/events.py
@@ -3,9 +3,11 @@ import discord
 from .client import Client
 
 bot = Client.bot
+game = discord.Game("with her phone") #sets the game the bot is currently playing
 
 class Events:
 	
 	@bot.event
 	async def on_ready():
 		print('Kotori-san is ready to go!')
+		await bot.change_presence(status=discord.Status.online, activity=game)
