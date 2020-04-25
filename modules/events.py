73a3444bffae766b3e69b81310b0505160db631e
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-04-24 20:56:56 -0500

    Set up the basic bot features, including the ability to read "token.txt" (located in the root directory) to gain access to the Discord bot.

--- modules/events.py
+++ modules/events.py
@@ -1,10 +1,11 @@
 import discord
 
-client = discord.Client()
+from .client import Client
+
+bot = Client.bot
 
 class Events:
 	
 	@bot.event
 	async def on_ready():
-		await bot.change_presence(status=discord.Status.online, activity=game)
 		print('Kotori-san is ready to go!')
