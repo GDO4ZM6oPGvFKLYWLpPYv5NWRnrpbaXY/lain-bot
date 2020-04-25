5e6cd4aa1d0bf71e799b341b062d62e11229b1eb
Tatsu <tatsu.eliason@gmail.com>
2020-04-24 20:26:10 -0500

    Revert "Initial discord.py integration"
    This reverts commit e41aee1e5e05ffc410e8695729f1a606f20379fb.

--- modules/client.py
+++ modules/client.py
@@ -1,10 +1,4 @@
 import discord
-from discord.ext import commands
-
-description = '''Test'''
 
 class Client:
-
-	bot = commands.Bot(command_prefix='k!', description=description)
-	
-	token = open("token.txt", "r")	
+	client = discord.Client()
