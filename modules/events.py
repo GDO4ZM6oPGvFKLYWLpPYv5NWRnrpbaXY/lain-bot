f5e12b180625d0b706fda0d7f467fea9e9725235
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-04-25 06:19:47 -0500

    improved searches

--- modules/events.py
+++ modules/events.py
@@ -17,15 +17,19 @@ class Events:
 		
 		channel = bot.get_channel(Config._botChannel)
 		
-		safebooruImageURL = Safebooru.booruSearch('otonashi_kotori 1girl')
+		safebooruImageURL = Safebooru.booruSearch('otonashi_kotori 1girl')[0]
+		safebooruPageURL = Safebooru.booruSearch('otonashi_kotori 1girl')[1]
+		safebooruTagsTogether = Safebooru.booruSearch('otonashi_kotori 1girl')[2]
 		
 		embed = discord.Embed(
 			title = 'こんにちは、プロデューサーさん！',
 			description = 'Kotori is now online!',
-			color = discord.Color.green()
+			color = discord.Color.green(),
+			url = safebooruPageURL
 		)
 		
 		embed.set_image(url=safebooruImageURL)
 		embed.set_author(name='音無小鳥', url='https://www.project-imas.com/wiki/Kotori_Otonashi', icon_url='https://raw.githubusercontent.com/SigSigSigurd/kotori-san-bot/master/avatar.png')
+		embed.set_footer(text=safebooruTagsTogether)
 			
 		await channel.send(embed=embed)
--- modules/safebooru.py
+++ modules/safebooru.py
@@ -17,14 +17,31 @@ class Safebooru:
 		randImage = random.randint(0, 99)
 		
 		
-		print(dir(r))
+		#print(dir(r))
 		
 		
 		randImageDirectory = r_dict[randImage]['directory']
 		randImageFile = r_dict[randImage]['image']
 		randImageID = r_dict[randImage]['id']
 		
-		randImageURL = 'https://safebooru.org//images/'+str(randImageDirectory)+'/'+str(randImageFile)+'?'+str(randImageID) #puts together the json info to find the image 
+		randTags = r_dict[randImage]['tags']
 		
-		print(randImageURL)
-		return(randImageURL)
+		randImageTags = randTags.split(" ")
+		
+		randImageURL = 'https://safebooru.org//images/'+str(randImageDirectory)+'/'+str(randImageFile)+'?'+str(randImageID) #puts together the json info to find the image 
+		randImagePage = 'https://safebooru.org/index.php?page=post&s=view&id='+str(randImageID)
+		
+		randomTagsCount = len(randImageTags)
+		
+		tagNumber = random.sample(range(1, randomTagsCount), 3)
+		
+		randTagOne = randImageTags[tagNumber[0]]
+		randTagTwo = randImageTags[tagNumber[1]]
+		randTagThree = randImageTags[tagNumber[2]]
+		
+		randTagsTogether = str(searchTerm+' | '+randTagOne+' | '+randTagTwo+' | '+randTagThree)
+		
+		returnList = [randImageURL, randImagePage, randTagsTogether]
+		
+		# print(randImageURL)
+		return(returnList)
