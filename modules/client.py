98681507f504108b0107c7be8a6bf05846063361
Tatsu <tatsu.eliason@gmail.com>
2020-04-25 15:56:52 -0500

    Added a global check for the server ID

--- modules/client.py
+++ modules/client.py
@@ -2,8 +2,16 @@ import discord
 from discord.ext import commands
 
 description = '''Test'''
- 
 
 class Client:
+
+	serverID = 0
+
 	bot = commands.Bot(command_prefix='k!', description=description) #sets up the bot
 	
+	@bot.check
+	async def check_serverID(ctx):
+		# global serverID
+		Client.serverID = ctx.guild.id
+		print('Server ID: '+str(Client.serverID))
+		return Client.serverID
--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,13 @@
 import discord
 
 from .client import Client
+
 bot = Client.bot
 
 class Config:
 	
-	_serverName = "Madison 春香 ファンClub"
-	_serverID = 554770485079179264
-	_botChannel = 554774183855521792
+	serverName = "Madison 春香 ファンClub"
+	serverID = Client.serverID
+	botChannel = 554774183855521792
+	
+	
