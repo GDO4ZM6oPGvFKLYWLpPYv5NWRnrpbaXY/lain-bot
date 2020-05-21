67c763d067a0d6d4d5a14ad5148650313940039e
TKakitsubata <tatsu.eliason@gmail.com>
2020-05-21 15:18:52 -0500

    Config branch

--- modules/client.py
+++ modules/client.py
@@ -8,10 +8,11 @@ class Client:
 	serverID = 0
 
 	bot = commands.Bot(command_prefix='k!', description=description) #sets up the bot
-	
+
 	@bot.check
 	async def check_serverID(ctx):
 		# global serverID
 		Client.serverID = ctx.guild.id
 		print('Global check complete! Server ID: '+str(Client.serverID))
+		serverID = client.serverID
 		return Client.serverID
