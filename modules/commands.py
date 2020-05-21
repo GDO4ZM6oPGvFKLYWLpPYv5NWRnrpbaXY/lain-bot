f1148b25c46b23201b998f89c7f55d8955b1f73e
TKakitsubata <tatsu.eliason@gmail.com>
2020-05-21 15:08:04 -0500

    Removes config.db and os module for anilist, so dumb shit doesn't happen

--- modules/commands.py
+++ modules/commands.py
@@ -17,32 +17,32 @@ class Commands:
 
 	@bot.command(pass_context=True)
 	async def safebooru(ctx, tags): #looks up images on safebooru
-	
+
 		channel = ctx.message.channel
-		
+
 		safebooruSearch = Safebooru.booruSearch(tags)
-		
+
 		safebooruImageURL = safebooruSearch[0]
 		safebooruPageURL = safebooruSearch[1]
 		safebooruTagsTogether = safebooruSearch[2]
-		
+
 		embed = discord.Embed(
 			title = tags,
 			description = 'Is this what you were looking for, producer?',
 			color = discord.Color.green(),
 			url = safebooruPageURL
 		)
-		
+
 		embed.set_image(url=safebooruImageURL)
 		embed.set_author(name='音無小鳥', url='https://www.project-imas.com/wiki/Kotori_Otonashi', icon_url='https://raw.githubusercontent.com/SigSigSigurd/kotori-san-bot/master/assets/search.png')
 		embed.set_footer(text=safebooruTagsTogether)
-		
+
 		await channel.send(embed=embed)
-	
+
 	@bot.command(pass_context=True)
 	async def serverID(ctx): #returns the serverID, mainly for debug purposes
 		await ctx.send('Server ID: '+str(Client.serverID))
-		
+
 	@bot.command(pass_context=True)
 	async def anilistTest(ctx, searchID):
 		anilistResults = Anilist.aniSearch(searchID)
--- modules/safebooru.py
+++ modules/safebooru.py
@@ -3,11 +3,11 @@ import os
 import random
 
 class Safebooru:
-	
+
 	def booruSearch(searchTerm):
 		r = requests.get("https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1&tags="+searchTerm)
 		r_dict = r.json()
-		
+
 		# file = open('test.json', 'w')
 		# for listitem in r_dict:
 			# file.write(str(listitem))
@@ -15,33 +15,33 @@ class Safebooru:
 		# file.close()
 
 		randImage = random.randint(0, len(r_dict)-1)
-		
-		
+
+
 		#print(dir(r))
-		
-		
+
+
 		randImageDirectory = r_dict[randImage]['directory']
 		randImageFile = r_dict[randImage]['image']
 		randImageID = r_dict[randImage]['id']
-		
+
 		randTags = r_dict[randImage]['tags']
-		
+
 		randImageTags = randTags.split(" ")
-		
-		randImageURL = 'https://safebooru.org//images/'+str(randImageDirectory)+'/'+str(randImageFile)+'?'+str(randImageID) #puts together the json info to find the image 
+
+		randImageURL = 'https://safebooru.org//images/'+str(randImageDirectory)+'/'+str(randImageFile)+'?'+str(randImageID) #puts together the json info to find the image
 		randImagePage = 'https://safebooru.org/index.php?page=post&s=view&id='+str(randImageID)
-		
+
 		randomTagsCount = len(randImageTags)
-		
+
 		tagNumber = random.sample(range(1, randomTagsCount), 3)
-		
+
 		randTagOne = randImageTags[tagNumber[0]]
 		randTagTwo = randImageTags[tagNumber[1]]
 		randTagThree = randImageTags[tagNumber[2]]
-		
+
 		randTagsTogether = str(searchTerm+' | '+randTagOne+' | '+randTagTwo+' | '+randTagThree)
-		
+
 		returnList = [randImageURL, randImagePage, randTagsTogether]
-		
+
 		# print(randImageURL)
 		return(returnList)
