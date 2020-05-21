56de7b78d29f619c4b89343f4dcbef081240171c
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-05-21 15:18:52 -0500

    Config branch

--- modules/config.py
+++ modules/config.py
@@ -8,7 +8,7 @@ from .client import Client
 bot = Client.bot
 
 class Config:
-	
+
 	# serverName = "Madison 春香 ファンClub"
 	# serverID = Client.serverID
-	botChannel = 554774183855521792
+	# botChannel = 554774183855521792
