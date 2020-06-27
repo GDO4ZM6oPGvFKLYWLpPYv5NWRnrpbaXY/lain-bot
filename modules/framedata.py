2403584375001de657f7cf9d8d6090d86cf82f5b
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-06-26 20:07:19 -0500

    Potential fix 2

--- modules/framedata.py
+++ modules/framedata.py
@@ -2,10 +2,11 @@ import json
 import os
 
 class Framedata():
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    print(os.getcwd())
 
     def search(game, char, move):
-        with open("./framedata/"+game+"/"+char+".json", "r") as frame_json:
+        with open(os.getcwd()+"/framedata/"+game+"/"+char+".json", "r") as frame_json:
             json_data = json.load(frame_json)
         for i in json_data:
             if i["Command"] == move:
