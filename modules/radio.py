24ccf17597dfdc18bf6a39d8854ad1a673577225
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2020-06-27 18:59:09 -0400

    listen to radio

---
+++ modules/radio.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+class Radio():
+    # radios on servers
+    players = {}
+
+    def information():
+        r = requests.get('https://r-a-d.io/api')
+        if r.status_code == 200:
+            m = r.json()
+        elif r.status_code == 400:
+            m = 'Connection error'
+        else:
+            m = 'Unknown error'
+        return m
