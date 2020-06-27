3a4232578e67ce1dda470420b7eb4bc42a925021
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-06-26 20:02:33 -0500

    Potential fix

--- modules/framedata.py
+++ modules/framedata.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 class Framedata():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
     def search(game, char, move):
         with open("./framedata/"+game+"/"+char+".json", "r") as frame_json:
             json_data = json.load(frame_json)
