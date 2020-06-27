e21c9b7c34764ead4402a316d35406e288494cb7
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-06-26 20:35:33 -0500

    Updates frame data code

--- modules/framedata.py
+++ modules/framedata.py
@@ -2,13 +2,14 @@ import json
 import os
 
 class Framedata():
-
-    print(os.getcwd())
-
-    def search(game, char, move):
+    def search(game, char, move: str):
         with open(os.getcwd()+"/framedata/"+game+"/"+char+".json", "r") as frame_json:
             json_data = json.load(frame_json)
         for i in json_data:
-            if i["Command"] == move:
-                return_move = i
+            #if i["Command"] == move:
+                #return_move = i
+            for comm in i["Command"]:
+                if comm.lower() == move.lower():
+                    return_move = i
+
         return return_move
