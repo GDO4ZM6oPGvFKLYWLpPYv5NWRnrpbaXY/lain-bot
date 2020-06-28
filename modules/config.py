59c845a4907b52285701009cf68ae40269d3ecb4
SigSigSigurd <tatsu.eliason@gmail.com>
2020-06-27 19:35:01 -0500

    Fixes some lines

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import os
 
 from .client import Client
 
@@ -14,17 +15,17 @@ class Config:
 	# called when the server config needs to be updated
 	def cfgUpdate(serverID, dict, entry):
 		# reads the server configuration json
-		with open("./config/"+serverID+".json", 'r') as server_json:
+		with open(os.getcwd()+"/config/"+serverID+".json", 'r') as server_json:
 			json_data = json.load(server_json)
 			# changes the line to update in the json
 			json_data[dict] = entry
 		# writes the changes to the json file
-		with open("./config/"+serverID+".json", 'w') as server_json:
+		with open(os.getcwd()+"/config/"+serverID+".json", 'w') as server_json:
 			server_json.write(json.dumps(json_data))
 
 	def cfgRead(serverID, dict):
 		try:
-			with open("./config/"+serverID+".json", 'r') as server_json:
+			with open(os.getcwd()+"/config/"+serverID+".json", 'r') as server_json:
 				json_data = json.load(server_json)
 				entry = json_data[dict]
 				return entry
