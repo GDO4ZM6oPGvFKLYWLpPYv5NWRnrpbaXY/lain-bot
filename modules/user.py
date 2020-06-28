8a37e0d27e036810c39ba1010a27fb324c7ce729
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-06-27 19:35:01 -0500

    Fixes some lines

--- modules/user.py
+++ modules/user.py
@@ -1,22 +1,23 @@
 import discord
 import json
+import os
 
 from .client import Client
 
 class User:
 	def userUpdate(userID, dict, entry):
 		# reads the user configuration json
-		with open("./user/"+userID+".json", 'r') as user_json:
+		with open(os.getcwd()+"/user/"+userID+".json", 'r') as user_json:
 			json_data = json.load(user_json)
 			# changes the line to update in the json
 			json_data[dict] = entry
 		# writes the changes to the json file
-		with open("./user/"+userID+".json", 'w') as user_json:
+		with open(os.getcwd()+"/user/"+userID+".json", 'w') as user_json:
 			user_json.write(json.dumps(json_data))
 
 	def userRead(userID, dict):
 		try:
-			with open("./user/"+userID+".json", 'r') as user_json:
+			with open(os.getcwd()+"/user/"+userID+".json", 'r') as user_json:
 				json_data = json.load(user_json)
 				entry = json_data[dict]
 				return entry
