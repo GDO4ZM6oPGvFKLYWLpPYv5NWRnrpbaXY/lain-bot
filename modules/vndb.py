a07dd47a817b1c203ac880a343da4baace1714a8
SigSigSigurd <tatsu.eliason@gmail.com>
2020-06-29 04:47:52 -0500

    Adds esports club verification

--- modules/vndb.py
+++ modules/vndb.py
@@ -6,22 +6,22 @@ class Vndb():
 		# setup socket and connection
 		self.sock = socket.socket()
 		self.sock.connect(('api.vndb.org', 19534))
-		
+
 		# login
 		info = {'protocol': 1, 'client': 'test', 'clientver': float(0.1)}
 		login = 'login ' + json.dumps(info) + '\x04'
 		self.sock.send(login.encode())
-		
+
 		finished = False
 		f = ''
 		while not finished:
 			f += self.sock.recv(4096).decode()
 			if '\x04' in f:
 				finished = True
-		
+
 		if f == 'error':
 			print('error')
-	
+
 	def vn(self, title):
 		# prepare command
 		whole = 'get vn basic,details,stats,screens (title ~ "{0}")'.format(title)
@@ -34,7 +34,7 @@ class Vndb():
 			whole += self.sock.recv(4096).decode()
 			if '\x04' in whole:
 				finished = True
-		
+
 		remove = whole.replace('\x04', '')
 		res = json.loads(remove.replace('results', ''))
 		return res
