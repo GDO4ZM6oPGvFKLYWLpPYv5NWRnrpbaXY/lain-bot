6d83c07f48a495f089338adecacac5deded186e3
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2020-06-30 03:46:52 -0400

    json fix

--- modules/commands.py
+++ modules/commands.py
@@ -414,7 +414,6 @@ class Commands:
 		
 		if found:
 			try:
-				await ctx.send(json.load(open('themes.json', 'r')))
 				parts = Themes.themesMoe(year, select, mal, t, num)
 				ctx.send(parts)
 				big = num
