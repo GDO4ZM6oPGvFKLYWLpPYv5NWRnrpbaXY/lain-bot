e7522bd4be11ac6de0d890ebacbd2b36cfc32c92
SigSigSigurd <tatsu.eliason@gmail.com>
2020-07-07 10:44:38 -0500

    essential commands

--- modules/commands/memes.py
+++ modules/commands/memes.py
@@ -8,12 +8,12 @@ bot = Client.bot
 
 class Memes:
 
-    @bot.group()
-    async def memes(ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send('Invalid xiii command passed...')
+    # @bot.group()
+    # async def memes(ctx):
+        # if ctx.invoked_subcommand is None:
+            # await ctx.send('Invalid xiii command passed...')
 
-    @memes.command(pass_context=True)
+    @bot.command(pass_context=True)
     async def momoko(ctx):
         with open(os.getcwd()+"/assets/memes/momoko.jpg", 'rb') as fp:
             await ctx.send(file=discord.File(fp, 'momoko.jpg'))
@@ -22,3 +22,8 @@ class Memes:
     async def simp(ctx):
         with open(os.getcwd()+"/assets/memes/simp.png", 'rb') as fp:
             await ctx.send(file=discord.File(fp, 'simp.png'))
+
+    @bot.command(pass_context=True)
+    async def kawamori(ctx):
+        with open(os.getcwd()+"/assets/memes/kawamori.png", 'rb') as fp:
+            await ctx.send(file=discord.File(fp, 'kawamori.png'))
