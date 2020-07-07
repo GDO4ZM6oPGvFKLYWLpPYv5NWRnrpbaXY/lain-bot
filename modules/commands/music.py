d0ccd031fd8b8c3d9b54efa3dff96c52ff4d9e6d
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2020-07-07 06:54:51 -0400

    undo change test

--- modules/commands/music.py
+++ modules/commands/music.py
@@ -358,14 +358,14 @@ async def play(ctx, voice, url):
 		qnum = len(queues)
 		if qnum != 0:
 			next = queues.pop()
-			voice.play(discord.FFmpegPCMAudio(next, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
+			voice.play(discord.FFmpegPCMAudio(next), after=lambda e: check_queue())
 		else:
 			queues.clear()
 
 	if voice.is_playing():
 		await add(ctx, url)
 	else:
-		voice.play(discord.FFmpegPCMAudio(url, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
+		voice.play(discord.FFmpegPCMAudio(url), after=lambda e: check_queue())
 
 queues = []
 
