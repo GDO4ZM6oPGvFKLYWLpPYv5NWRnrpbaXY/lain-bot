d6c73e5cc2564c63293527c519f546863dae98c5
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2020-07-07 08:38:06 -0400

    send ffmpeg error and format code

--- modules/commands/music.py
+++ modules/commands/music.py
@@ -16,7 +16,7 @@ class Music(commands.Cog):
     @bot.group()
     async def radio(ctx):
         if ctx.invoked_subcommand is None:
-                await ctx.send('Invalid radio command passed...')
+            await ctx.send('Invalid radio command passed...')
 
     @radio.command(pass_context=True)
     async def start(ctx):
@@ -34,17 +34,19 @@ class Music(commands.Cog):
             song = main['np']
             q = main['queue']
             djname = main['dj']['djname']
-            djimage = 'https://r-a-d.io/api/dj-image/' + str(main['dj']['djimage'])
+            djimage = 'https://r-a-d.io/api/dj-image/' + \
+                str(main['dj']['djimage'])
             bitrate = str(main['bitrate'])
             listeners = str(main['listeners'])
 
             embed = discord.Embed(
-                    title = 'R/a/dio Information',
-                    color = discord.Color.red(),
-                    url = 'https://r-a-d.io/'
-                )
+                title='R/a/dio Information',
+                color=discord.Color.red(),
+                url='https://r-a-d.io/'
+            )
 
-            embed.set_author(name=djname, url='https://r-a-d.io/staff', icon_url=djimage)
+            embed.set_author(
+                name=djname, url='https://r-a-d.io/staff', icon_url=djimage)
 
             # now playing
             embed.add_field(name='Now Playing', value=song, inline=False)
@@ -57,7 +59,8 @@ class Music(commands.Cog):
                 i += 1
             embed.add_field(name='Queue', value=out, inline=False)
 
-            embed.set_footer(text='Listeners: {0}, Bitrate: {1}'.format(listeners, bitrate), icon_url='https://r-a-d.io/assets/logo_image_small.png')
+            embed.set_footer(text='Listeners: {0}, Bitrate: {1}'.format(
+                listeners, bitrate), icon_url='https://r-a-d.io/assets/logo_image_small.png')
 
             await ctx.send(embed=embed)
         except:
@@ -79,24 +82,27 @@ class Music(commands.Cog):
                 info = ydl.extract_info(url, download=False)
 
                 if '_type' in info and info['_type'] == 'playlist':
-                    #await ctx.send('playlist not currently supported, playing first video...')
-                    info = ydl.extract_info(info['entries'][0]['url'], download=False)
-                
+                    # await ctx.send('playlist not currently supported, playing first video...')
+                    info = ydl.extract_info(
+                        info['entries'][0]['url'], download=False)
+
                 embed = discord.Embed(
-                    title = info['title'],
-                    color = discord.Color.red(),
-                    url = info['webpage_url']
+                    title=info['title'],
+                    color=discord.Color.red(),
+                    url=info['webpage_url']
                 )
 
-                embed.set_author(name=info['uploader'], url=info['channel_url'])
+                embed.set_author(
+                    name=info['uploader'], url=info['channel_url'])
                 embed.set_thumbnail(url=info['thumbnails'][0]['url'])
-                embed.set_footer(text='YouTube', icon_url='https://www.thermalwoodcanada.com/images/youtube-play-button-transparent-background-4.png')
+                embed.set_footer(
+                    text='YouTube', icon_url='https://www.thermalwoodcanada.com/images/youtube-play-button-transparent-background-4.png')
                 await ctx.send(embed=embed)
 
                 await join(ctx, info['formats'][3]['url'])
         except Exception as e:
             await ctx.send(str(e))
-    
+
     @bot.command(pass_context=True, aliases=['leave', 'radio stop', 'radio leave'])
     async def stop(ctx):
         try:
@@ -213,17 +219,20 @@ class Music(commands.Cog):
         elif romaji == 'None':
             romaji == english
 
-        parts = Themes.search(english.lower(), romaji.lower(), sId, show, select, songs)
+        parts = Themes.search(
+            english.lower(), romaji.lower(), sId, show, select, songs)
         found = False
         try:
             embed = discord.Embed(
-                    title = parts['big'],
-                    color = discord.Color.orange(),
-                    url = parts['video']
-                )
+                title=parts['big'],
+                color=discord.Color.orange(),
+                url=parts['video']
+            )
 
-            embed.set_author(name=parts['title'], url=showUrl, icon_url=showPic)
-            embed.set_footer(text=parts['op/ed'], icon_url='https://openings.moe/assets/logo/512px.png')
+            embed.set_author(name=parts['title'],
+                             url=showUrl, icon_url=showPic)
+            embed.set_footer(
+                text=parts['op/ed'], icon_url='https://openings.moe/assets/logo/512px.png')
             await ctx.send(embed=embed)
 
             await join(ctx, parts['video'])
@@ -236,12 +245,13 @@ class Music(commands.Cog):
                 parts = Themes.themesMoe(year, mal, t, num)
                 big = num
                 embed = discord.Embed(
-                        title = parts['name'],
-                        color = discord.Color.orange(),
-                        url = parts['video']
+                    title=parts['name'],
+                    color=discord.Color.orange(),
+                    url=parts['video']
                 )
                 embed.set_author(name=romaji, url=showUrl, icon_url=showPic)
-                embed.set_footer(text=select, icon_url='https://external-content.duckduckgo.com/ip3/themes.moe.ico')
+                embed.set_footer(
+                    text=select, icon_url='https://external-content.duckduckgo.com/ip3/themes.moe.ico')
                 await ctx.send(embed=embed)
 
                 await join(ctx, parts['video'])
@@ -295,13 +305,15 @@ class Music(commands.Cog):
         found = False
         try:
             embed = discord.Embed(
-                    title = parts['big'],
-                    color = discord.Color.orange(),
-                    url = parts['video']
-                )
+                title=parts['big'],
+                color=discord.Color.orange(),
+                url=parts['video']
+            )
 
-            embed.set_author(name=parts['title'], url=showUrl, icon_url=showPic)
-            embed.set_footer(text=parts['op/ed'], icon_url='https://openings.moe/assets/logo/512px.png')
+            embed.set_author(name=parts['title'],
+                             url=showUrl, icon_url=showPic)
+            embed.set_footer(
+                text=parts['op/ed'], icon_url='https://openings.moe/assets/logo/512px.png')
             await ctx.send(embed=embed)
 
             await join(ctx, parts['video'])
@@ -314,12 +326,13 @@ class Music(commands.Cog):
                 parts = Themes.themesMoe(year, mal, t, num)
                 big = num
                 embed = discord.Embed(
-                        title = parts['name'],
-                        color = discord.Color.orange(),
-                        url = parts['video']
+                    title=parts['name'],
+                    color=discord.Color.orange(),
+                    url=parts['video']
                 )
                 embed.set_author(name=romaji, url=showUrl, icon_url=showPic)
-                embed.set_footer(text=select, icon_url='https://external-content.duckduckgo.com/ip3/themes.moe.ico')
+                embed.set_footer(
+                    text=select, icon_url='https://external-content.duckduckgo.com/ip3/themes.moe.ico')
                 await ctx.send(embed=embed)
 
                 await join(ctx, parts['video'])
@@ -329,65 +342,71 @@ class Music(commands.Cog):
 
 # join a voice channel and play link
 async def join(ctx, url):
-	global voice
-	try:
-		# get voice channel
-		channel = ctx.author.voice.channel
-		if channel != None:
-			# join channel
-			voice = get(bot.voice_clients, guild=ctx.guild)
+    global voice
+    try:
+        # get voice channel
+        channel = ctx.author.voice.channel
+        if channel != None:
+            # join channel
+            voice = get(bot.voice_clients, guild=ctx.guild)
 
-			if voice and voice.is_connected():
-				# play music
-				Radio.players[ctx.guild.id] = voice
+            if voice and voice.is_connected():
+                # play music
+                Radio.players[ctx.guild.id] = voice
 
-			else:
-				voice = await channel.connect()
+            else:
+                voice = await channel.connect()
 
-			await play(ctx, voice, url)
-	except AttributeError as a:
-		print(a)
-		await ctx.send('User is not in a channel.')
-	except Exception as e:
-		print(e)
-		await ctx.send('Unexpected error')
+            await play(ctx, voice, url)
+    except AttributeError as a:
+        print(a)
+        await ctx.send('User is not in a channel.')
+    except Exception as e:
+        print(e)
+        await ctx.send('Unexpected error')
 
 async def play(ctx, voice, url):
-	def check_queue():
-		qnum = len(queues)
-		if qnum != 0:
-			next = queues.pop()
-			voice.play(discord.FFmpegPCMAudio(next, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
-		else:
-			queues.clear()
+    def check_queue():
+        qnum = len(queues)
+        if qnum != 0:
+            next = queues.pop()
+            voice.play(discord.FFmpegPCMAudio(
+                next, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
+        else:
+            queues.clear()
 
-	if voice.is_playing():
-		await add(ctx, url)
-	else:
-		voice.play(discord.FFmpegPCMAudio(url, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
+    if voice.is_playing():
+        await add(ctx, url)
+    else:
+        try:
+            voice.play(discord.FFmpegPCMAudio(
+                url, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}), after=lambda e: check_queue())
+        except Exception as e:
+            await ctx.send(e)
 
 queues = []
 
 async def add(ctx, url):
-	queues.insert(0, url)
+    queues.insert(0, url)
 
-	await ctx.send('**{0}** in queue'.format(len(queues)))
+    await ctx.send('**{0}** in queue'.format(len(queues)))
 
 # helper functions for vn and anilist search
 def parse(ctx, num):
-		# determine which op/ed should be played
-		show = ''
-		try:
-			int(num[0])
-			# get show name
-			show = str(ctx.message.content)[(len(ctx.prefix) + len('op ' + str(num) + ' ')):]
-		except ValueError as v:
-			print(v)
-			if len(num) >= 2:
-				num = '1'
-				show = str(ctx.message.content)[(len(ctx.prefix) + len('op ')):]
-				#await ctx.send('Choose which OP you want to play first (1, 2, 3...)')
-		except IndexError as f:
-			print(f)
+    # determine which op/ed should be played
+    show = ''
+    try:
+        int(num[0])
+        # get show name
+        show = str(ctx.message.content)[
+            (len(ctx.prefix) + len('op ' + str(num) + ' ')):]
+    except ValueError as v:
+        print(v)
+        if len(num) >= 2:
+            num = '1'
+            show = str(ctx.message.content)[(len(ctx.prefix) + len('op ')):]
+            # await ctx.send('Choose which OP you want to play first (1, 2, 3...)')
+    except IndexError as f:
+        print(f)
 
-		return {'show' : show, 'num' : num}
+    return {'show': show, 'num': num}
