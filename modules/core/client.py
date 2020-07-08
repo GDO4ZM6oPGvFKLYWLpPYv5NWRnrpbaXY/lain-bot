6d688b22633b6b7d00a14d97a7c24914e80aa571
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-07-07 23:45:02 -0500

    typo

--- modules/core/client.py
+++ modules/core/client.py
@@ -12,7 +12,7 @@ class Client:
 	prefix = ">"
 	bot = commands.Bot(command_prefix=prefix, description=description) #sets up the bot
 
-	al_json_path = os.getenv(OS_PATH)+"/modules/anime/config/alID.json"
+	al_json_path = os.getenv("OS_PATH")+"/modules/anime/config/alID.json"
 	al_json = json.load(open(al_json_path, 'r'))
 
 	# @bot.check
