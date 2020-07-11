26a92d414cfdf664fb334efabec30afbda5f39b9
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-07-10 22:00:14 -0500

    Update mainesports.py

--- mainesports.py
+++ mainesports.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import logging
 
 from modules.core.client import Client
-from modules.core.command import Command
 from modules.core.events import Events
 from modules.core.cogs import Cogs
 from modules.esports.esportsclub import EsportsClub # for commands / features for UW-Madison Esports Club
