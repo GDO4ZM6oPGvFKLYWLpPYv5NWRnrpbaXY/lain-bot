5e71c8ebdb429aa741d456a37a82e0916a9aeb6f
Anthony <avanlent@protonmail.com>
2020-09-15 20:26:30 -0500

    cleanup

--- modules/core/client.py
+++ modules/core/client.py
@@ -1,20 +1,6 @@
-import discord
 from discord.ext import commands
-import os
-import json
-
-description = '''Test'''
 
 class Client:
 
-	serverID = 0
-
 	prefix = ">"
-	bot = commands.Bot(command_prefix=prefix, description=description) #sets up the bot
-
-	# @bot.check
-	# async def check_serverID(ctx):
-		# global serverID
-		# Client.serverID = ctx.guild.id
-		# print('Global check complete! Server ID: '+str(Client.serverID))
-		# return Client.serverID
+	bot = commands.Bot(command_prefix=prefix) #sets up the bot
