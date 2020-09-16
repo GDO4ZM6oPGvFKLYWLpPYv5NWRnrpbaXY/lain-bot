361a41bea85ab26ca5936592176bb69898fd79c9
Anthony <avanlent@protonmail.com>
2020-09-16 13:25:19 -0500

    updated 'al user set' command

--- modules/anime/anilist2.py
+++ modules/anime/anilist2.py
@@ -82,10 +82,80 @@ class Anilist2:
         }
         '''
 
-    userNameQuery = '''
-        query($id: Int) {
-            User(id: $id) {
+    userSearchQuery = '''
+        query($name: String) {
+            User(name:$name) {
                 name
+                id
+                mediaListOptions {
+                    scoreFormat
+                }
+                about
+                avatar {
+                    large
+                }
+                bannerImage
+                favourites {
+                    anime {
+                        nodes {
+                            title {
+                                romaji
+                            }
+                        }
+                    }
+                }
+                statistics {
+                    anime {
+                        count
+                        meanScore
+                        genres(limit:5, sort:COUNT_DESC) {
+                            genre
+                        }
+                    }
+                }
+                updatedAt
+            }
+            animeList: MediaListCollection(userName:$name, type:ANIME){
+                lists {
+                    entries {
+                        status
+                        mediaId
+                        score
+                        progress
+                        media {
+                            bannerImage
+                            coverImage {
+                                large
+                            }
+                            episodes
+                            title {
+                                romaji
+                            }
+                        }
+                    }
+                }
+            }
+            mangaList: MediaListCollection(userName:$name, type:MANGA){
+                lists {
+                    entries {
+                        status
+                        mediaId
+                        score
+                        progress
+                        progressVolumes
+                        media {
+                            bannerImage
+                            coverImage {
+                                large
+                            }
+                            chapters
+                            volumes
+                            title {
+                                romaji
+                            }
+                        }
+                    }
+                }
             }
         }
         '''
@@ -93,4 +163,10 @@ class Anilist2:
 # TODO error checks
     async def getUserData(session, id):
         async with session.post(Anilist2.apiUrl, json={'query': Anilist2.userDataQuery, 'variables': {'id': id}}) as resp:
+            return await resp.json()
+
+    async def userSearch(session, name):
+        if not name:
+            return
+        async with session.post(Anilist2.apiUrl, json={'query': Anilist2.userSearchQuery, 'variables': {'name': name}}) as resp:
             return await resp.json()
