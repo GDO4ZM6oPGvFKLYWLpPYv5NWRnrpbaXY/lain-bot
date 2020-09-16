51fd73f367b7d2518679c49b5ff3eadc9e30c37b
Anthony <avanlent@protonmail.com>
2020-09-15 20:46:49 -0500

    add core functionality of asynchronous http calls, mongodb db connection, and image generator

---
+++ modules/core/session.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import aiohttp, asyncio
+
+class Session(commands.Cog):
+
+	def __init__(self, bot):
+		self.bot = bot
+		self.session = aiohttp.ClientSession()
+
+	async def close_session(self):
+		await self.session.close()
+
+	def cog_unload(self):
+		tmp_loop = asyncio.get_event_loop()
+		tmp_loop.run_until_complete(self.close_session(self))
+		tmp_loop.close()
