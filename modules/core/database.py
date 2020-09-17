1fbb799dde6c9f77ff2603649c10573af9d7fe1e
kallidusa <kallidusa@protonmail.com>
2020-09-17 14:38:56 -0500

    reduced complexity of updater

--- modules/core/database.py
+++ modules/core/database.py
@@ -3,9 +3,28 @@ import motor.motor_asyncio, os
 class Database:
 	client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://'+os.getenv('DBUSER')+':'+os.getenv('DBKEY')+'@cluster0.n52hk.gcp.mongodb.net/lain-bot?retryWrites=true&w=majority')
 
+	"""
+	user model:
+		_id				mongodb generated id
+		discordId		int - discord id for user
+		anilistId		int - anilist id
+		anilistName		str - anilist name
+		profile			{} - anilist profile data (genres, favs, etc)
+		animeList		{} - anilist animelist
+		mangaList		{} - anilist mangalist
+		status			int: 0=disabled(removed), 1=enabled(set)
+	"""
 	def userCollection():
 		return Database.client['lain-bot']['users']
-
+	
+	"""
+	guild model:
+		_id						mongodb generated id	
+		id						int - discord id for guild
+		animeMessageChannels	[int] - channel ids where anime messages are enabled
+		mangaMessageChannels	[int] - channel ids where manga messages are enabled
+		name					str - name of guild (not really needed, just there for debugging)
+	"""
 	def guildCollection():
 		return Database.client['lain-bot']['guilds']
 
@@ -19,7 +38,6 @@ class Database:
 		else:
 			return await d.find_one({'discordId':discordId})
 
-	# type either 'anime' or 'manga'
 	def formListEntryFromAnilistEntry(anilistEntry, anime=True):
 		new_entry = {
 			'status': anilistEntry['status'],
