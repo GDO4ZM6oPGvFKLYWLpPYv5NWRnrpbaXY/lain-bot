df1fcb267bda58d743570a38504adf401c49dbf4
Andrew Froemming <afroemming@pm.me>
2020-11-05 00:19:32 -0600

    use mentions instead of looking up names

--- modules/core/client.py
+++ modules/core/client.py
@@ -1,12 +1,9 @@
-import discord
 from discord.ext import commands
 from modules.core.session import Session
 
 class Client:
-	intents = discord.Intents.default()
-	intents.members = True 
 	
 	session = Session(raise_for_status=True)
 	
 	prefix = ">"
-	bot = commands.Bot(command_prefix=prefix, intents=intents) #sets up the bot
+	bot = commands.Bot(command_prefix=prefix) #sets up the bot
