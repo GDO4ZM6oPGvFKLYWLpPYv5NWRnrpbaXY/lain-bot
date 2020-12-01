19b34ca1e63668c11aa23b36de60a36f9a88a6e6
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-12-01 11:11:13 -0600

    Update updater.py

--- modules/anime/updater.py
+++ modules/anime/updater.py
@@ -50,7 +50,13 @@ class Updater(commands.Cog):
 
         for lst in fetched_list['lists']:
             for fetched_entry in lst['entries']:
-                if "Hentai" not in fetched_entry['media']['genres']:
+                # temp
+                try:
+                    genres = fetched_entry['media']['genres']
+                except:
+                    genres = None
+
+                if "Hentai" not in genres or genres==None:
                 # set up data differences
                     old_entry = old_list.get(str(fetched_entry['mediaId']))
                     if not old_entry:
@@ -148,8 +154,13 @@ class Updater(commands.Cog):
 
         for lst in fetched_list['lists']:
             for fetched_entry in lst['entries']:
-                # TEMP hentai filter
-                if "Hentai" not in fetched_entry['media']['genres']:
+                # temp
+                try:
+                    genres = fetched_entry['media']['genres']
+                except:
+                    genres = None
+
+                if "Hentai" not in genres or genres==None:
                     # set up data differences
                     old_entry = old_list.get(str(fetched_entry['mediaId']))
                     if not old_entry:
