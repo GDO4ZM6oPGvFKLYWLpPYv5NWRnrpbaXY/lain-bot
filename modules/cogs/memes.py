a8dcda52919b94f8a36935795ee0e9cd6b275516
AnemoneAmenome <38965835+AnemoneAmenome@users.noreply.github.com>
2020-12-03 13:18:12 -0600

    Cagalli Command

--- modules/cogs/memes.py
+++ modules/cogs/memes.py
@@ -33,9 +33,9 @@ class Memes(commands.Cog):
             # await ctx.send('Invalid xiii command passed...')
 
     @commands.command(pass_context=True)
-    async def momoko(self, ctx):
-        with open(os.getcwd()+"/assets/memes/momoko.jpg", 'rb') as fp:
-            await ctx.send(file=discord.File(fp, 'momoko.jpg'))
+    async def cagalli(self, ctx):
+        with open(os.getcwd()+"/assets/memes/cagalli.jpg", 'rb') as fp:
+            await ctx.send(file=discord.File(fp, 'cagalli.jpg'))
 
     @commands.command(pass_context=True)
     async def simp(self, ctx):
@@ -71,7 +71,7 @@ class Memes(commands.Cog):
     async def milky(self, ctx):
         await ctx.send("Milky Holmes represents the pandora'x box of the current century. Upon opening this metaphysical masterpiece you are greeted with a look directly into the inner depth of the human psyche. Every neuron fires at once, every muscle contracts at once, your retinas are bombarded with images and sound and smell and emotion and the wind swirls around you like a tornado as you begin to glow as if you were self immolating. Milky and Holmes are two things that should never logically go together and yet here we are. Now you may have guessed, \"Holmes\" refers to Holmes as in Sherlock Holmes, the master detective and famous crack addict; Milky Holmes decided to focus more on the crack addict part than any interpretation before. The pacing, the music, the art direction, it all elicits a feeling that I can only compare to what I imagine being hopped on those white rocks is like while the lights strobe and the music blares, what a life we live on this planet. Kiss your wife goodbye, tell your kids you love them, bone your dog goodbye, and get ready to walk the yellow brick plank, we're headed to a quadrochomatic party that never leaves your head.")
         await ctx.send("Allow me to set the stage. It's 3:00 AM, you're lying awake at night waiting for your brain to collapse in on itself so that you can finally escape this post modernist world for 3 or 4 hours. Suddenly, during your 4th prayer of the night a hole opens up in your vision from which a welcoming hand is extended. It hands you a letter with a beautiful hand pressed wax stamp, you open this letter and written by the great lord himself reads \"shove your fingers up your ass.\" This is what watching milky holmes is like. The devout understand, the sinners understand, but only true artists can appreciate the lengths that Milk and her homies will go for a laugh. When Ghandi said that the secret to true peace lies within ones soul, he meant this show's soul. Not you, not him, this is the one show who's soul can grant you inner peace in these trying times. After completing this 13 episode transcendental journey I was awarded with a PHD in modern philosophy and I was never bothered by the entropy of the universe again. In essence I became the closest being to god that exists, as I am now above that which I do not understand nor have a need to understand.")
- 
+
     @commands.command(pass_context=True)
     async def good(self, ctx, *args):
         if len(args) and args[0] in ['bot', 'bot!']:
@@ -96,16 +96,13 @@ class Memes(commands.Cog):
         except ClientResponseError:
             await ctx.send("Couldn't get profile picture.")
             return
-            
-        coords = ((242,58,370,186), (253,60,381,188), 
+
+        coords = ((242,58,370,186), (253,60,381,188),
             (263,68,391,196), (270,68,398,196), (270,73,398,201),
             (272,73,400,201), (269,78,397,206), (270,83,398,211),
             (270,83,398,211), (270,83,398,211), (64,90,192,218),
-            (32,70,160,198), (32,70,160,198))    
-        
+            (32,70,160,198), (32,70,160,198))
+
         base_img = os.getcwd() + '/assets/memes/punch.gif'
         with insert_picture_in_gif(base_img, user_pf, coords) as image_bin:
             await ctx.send(file=discord.File(image_bin, filename='image.gif'))
-
-        
-
