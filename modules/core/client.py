1062fb63ea81606dd560187b91ac30740a6f2b20
TatsuEliason <tatsu.eliason@gmail.com>
2021-02-18 13:57:23 -0600

    Remove deprecated "fighting" module

--- modules/core/client.py
+++ modules/core/client.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 
 from modules.cogs.anime import Anime
 from modules.cogs.music import Music
-from modules.cogs.fighting import Fighting
 from modules.cogs.configuration import Configuration
 from modules.cogs.memes import Memes
 from modules.cogs.animeclub import AnimeClub
@@ -20,13 +19,12 @@ intents.voice_states = True
 
 prefix = ">"
 
-class Client:	
+class Client:
     bot = commands.Bot(command_prefix=prefix, intents=intents) #sets up the bot
 
     Service.register(bot)
 
     bot.add_cog(Memes(bot))
-    bot.add_cog(Fighting(bot))
     bot.add_cog(Anime(bot))
     bot.add_cog(Configuration(bot))
     bot.add_cog(Music(bot))
