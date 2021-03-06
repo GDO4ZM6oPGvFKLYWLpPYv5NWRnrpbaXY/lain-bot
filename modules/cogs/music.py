9ab423aead61381bfcdfba8bc62519e83e76409b
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2021-03-06 03:38:45 -0500

    debug message

--- modules/cogs/music.py
+++ modules/cogs/music.py
@@ -184,7 +184,7 @@ class Music(commands.Cog):
         # 1 = opening
         t = 1
         build = parse(ctx, num)
-
+        await ctx.send('fix')
         # show to search for
         show = build['show']
         # which opening to pick
