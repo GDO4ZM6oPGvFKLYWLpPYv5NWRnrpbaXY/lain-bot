d4646e74c6ae37f44c1a746f605ba3b7e72254d6
avanlent <avanlent@protonmail.com>
2021-03-28 17:14:17 -0500

    use better looking help embed

--- modules/core/client.py
+++ modules/core/client.py
@@ -20,7 +20,15 @@ intents.voice_states = True
 
 prefix = ">"
 
-class Client:
+class CustomHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=16711680, description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+class Client:	
     bot = commands.Bot(command_prefix=prefix, intents=intents) #sets up the bot
 
     Service.register(bot)
@@ -32,3 +40,5 @@ class Client:
     bot.add_cog(AnimeClub(bot))
     bot.add_cog(Jisho(bot))
     bot.add_cog(Daijoubu(bot))
+
+    bot.help_command = CustomHelpCommand()
