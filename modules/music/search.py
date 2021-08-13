56968944ed6681718e7f5371f8c913b6fc4b27ea
Anthony VanLent <avanlent@protonmail.com>
2021-08-12 20:04:51 -0600

    Use better song search api and have song selection for >songs

---
+++ modules/music/search.py
@@ -0,0 +1,177 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Union, List, Optional
+
+import json, subprocess, json
+from fuzzywuzzy import process
+
+class SongVariant:
+    __slots__ = ['_kind', '_sequence', '_version']
+
+    def __init__(self, kind: str, sequence: Optional[int] = None, version: Optional[int] = None) -> None:
+        self._kind = kind
+        self._sequence = sequence if sequence and sequence != "None" else ''
+        self._version = version if version and version != "None" else ''
+
+    @property
+    def kind(self) -> str:
+        return self._kind
+
+    @property
+    def sequence(self) -> int:
+        return self._sequence
+
+    @property
+    def version(self) -> int:
+        return self._version
+
+    def __repr__(self) -> str:
+        return f"<{', '.join(map(lambda s: f'{str(s)[1:]}={getattr(self, s)}', self.__slots__))}>"
+
+    def __str__(self) -> str:
+        tmp = f"{self.kind}{self.sequence}"
+        if self.version:
+            tmp += f" V{self.version}"
+
+        return tmp
+
+class Song():
+    __slots__ = ['_variant', '_title', '_url', '_artists', '_flags']
+
+    def __init__(self, variant: SongVariant, title: str, url: str, artists: Optional[List[str]] = None, flags: Optional[List[str]] = None):
+        self._variant = variant
+        self._title = title
+        self._url = url
+        self._artists = artists if artists else []
+        self._flags = flags if flags else []
+
+    @property
+    def variant(self) -> SongVariant:
+        return self._variant
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def artists(self) -> List[str]:
+        return self._artists
+
+    def artists_str(self) -> str:
+        return f"{', '.join(self.artists)}"
+
+    @property
+    def flags(self) -> List[str]:
+        return self._flags
+
+    def __repr__(self) -> str:
+        return f"<{', '.join(map(lambda s: f'{str(s)[1:]}={getattr(self, s)}', self.__slots__))}>"
+
+    def __str__(self) -> str:
+        return f"[{str(self.variant)}] {self.title}"
+
+class Anime():
+    __slots__ = ['_title', '_url', '_cover', '_songs']
+
+    def __init__(self, title: str, url: Optional[str] = None, cover: Optional[str] = None, songs: Optional[List[Song]] = None):
+        self._title = title
+        self._url = url if url and url != 'None' else 'https://anilist.co/search/anime'
+        self._cover = cover if cover and cover != 'None' else 'https://files.catbox.moe/wgrm4k.png'
+        self._songs = songs if songs else []
+
+    @property
+    def title(self) -> str:
+        return self._title
+    
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def cover(self) -> str:
+        return self._cover
+
+    @property
+    def songs(self) -> List[Song]:
+        return self._songs
+
+    def __repr__(self) -> str:
+        return f"<{', '.join(map(lambda s: f'{str(s)[1:]}={getattr(self, s)}', self.__slots__))}>"
+
+
+class Themes():
+
+    class ThemesError(Exception):
+        def __init__(self, status=000, message="Generic Themes Error"):
+            self.message = message
+            self.status = status
+            super().__init__(self.message)
+    
+    class NoResultsError(Exception):
+        def __init__(self, status=204, message="No Results"):
+            self.message = message
+            self.status = status
+            super().__init__(self.message)
+
+    @staticmethod
+    def search_animethemesmoe(show):
+        url = f"https://staging.animethemes.moe/api/search?q={show}&include[anime]=themes.entries.videos,resources,images,themes.song.artists&fields[anime]=name&fields[search]=anime&fields[resource]=link&fields[theme]=type,sequence&fields[entry]=version,nsfw,spoiler&fields[video]=basename&fields[image]=link&fields[song]=title&fields[artist]=name"
+        
+        try:
+            res = subprocess.Popen(f"curl --silent \"{url}\"", stdout = subprocess.PIPE, shell=True)
+        except:
+            raise Themes.ThemesError(message="Err: curl subprocess error")
+        
+        try:
+            res = json.loads(res.communicate()[0].decode())
+        except:
+            raise Themes.ThemesError(message="Err: curl response decoding error")
+
+        if not res:
+            raise Themes.ThemesError(message="Err: empty response from animethemes.moe")
+
+        if "errors" in res:
+            raise Themes.ThemesError(status=res["errors"][0]["status"], message=res["errors"][0]["detail"])
+
+        data = res["search"]["anime"]
+
+        if not data:
+            raise Themes.NoResultsError()
+
+        try:
+            data = process.extractOne({"name":show}, data, lambda d: d["name"])[0]
+            songs = []
+            for theme in data["themes"]:
+                kind = theme["type"]
+                num = theme["sequence"]
+                title = theme["song"]["title"]
+                artists = [a["name"] for a in theme["song"]["artists"]]
+                for song in theme["entries"]:
+                    flags = []
+                    if song["nsfw"]:
+                        flags.append("NSFW")
+                    if song["spoiler"]:
+                        flags.append("Spoiler")
+                    songs.append(
+                        Song(
+                            SongVariant(kind, num, song["version"]),
+                            title,
+                            f"https://animethemes.moe/video/{song['videos'][0]['basename']}",
+                            artists,
+                            flags
+                        )
+                    )
+        except:
+            raise Themes.ThemesError(status=-1, message="Err: animethemes.moe response structure corrupt")
+
+        return Anime(
+            data["name"],
+            data["resources"][1]['link'],
+            data["images"][-1]['link'],
+            songs
+        )
